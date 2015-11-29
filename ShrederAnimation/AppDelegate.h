563badb6481679b0741e49d41500e5995528d3c8
hhuang <hhuang@microstrategy.com>
2015-11-29 12:05:07 +0800

    Initial Commit

---
+++ ShrederAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShrederAnimation
+//
+//  Created by Huang Hongsen on 15/11/29.
+//  Copyright © 2015年 cn.daniel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
