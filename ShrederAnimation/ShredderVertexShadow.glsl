dcc9b0c1842adb54942cc9087b8db18d737c7443
hhuang <hhuang@microstrategy.com>
2015-12-14 16:18:08 +0800

    Working

--- ShrederAnimation/ShredderVertexShadow.glsl
+++ ShrederAnimation/ShredderVertexShadow.glsl
@@ -8,7 +8,7 @@ layout(location = 2) in vec2 a_texCoord;
 
 out vec2 v_texCoord;
 out vec3 v_normal;
-out vec4 v_position;
+out vec2 v_position;
 
 #define M_PI 3.14159265358979323846264338327950288
 
@@ -16,5 +16,5 @@ void main() {
     gl_Position = u_mvpMatrix * a_position;
     v_texCoord = a_texCoord;
     v_normal = a_normal;
-    v_position = a_position;
+    v_position = a_position.xy;
 }
